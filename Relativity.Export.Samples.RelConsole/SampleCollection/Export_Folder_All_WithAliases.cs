cb81a7334bcc6674d3a98aaf15373ba08a6c7192
Igor Bołuchowski <igor.boluchowski@relativity.com>
2025-02-18 14:40:03 +0100

    REL-1018089 Added sample that uses aliases

---
+++ Relativity.Export.Samples.RelConsole/SampleCollection/Export_Folder_All_WithAliases.cs
@@ -0,0 +1,213 @@
+using Relativity.Export.Samples.RelConsole.Helpers;
+using Relativity.Export.V1.Builders.ExportSettings;
+using Relativity.Export.V1.Model.ExportJobSettings;
+
+namespace Relativity.Export.Samples.RelConsole.SampleCollection;
+
+public partial class BaseExportService
+{
+    [SampleMetadata(nameof(Export_FromFolder_All_WithAliases), "Exports native, fulltext, images and PDF files from folder and uses field aliases")]
+    public async Task Export_FromFolder_All_WithAliases()
+    {
+        // Your workspace ID.
+        // This is where we point to the workspace where we want to export from.
+        int workspaceID = 1020245;
+
+        // Your View ID.
+        // View will provide us with available data to export, requires folder to be visible there.
+        int viewID = 1042326;
+
+        // Your Folder ID.
+        // Our targetted folder. If you want to export from the workspace root, 
+        // the ID is different from the workspace ID.
+        int folderID = 1003697;
+
+        // Job related data
+        Guid jobID = Guid.NewGuid();
+        string? applicationName = "Export-Service-Sample-App";
+        string? correlationID = "Sample-Job-0001";
+
+        _logger.PrintSampleData(new Dictionary<string, string>
+        {
+            {"Workspace ID", workspaceID.ToString() },
+            {"View ID", viewID.ToString() },
+            {"Folder ID", folderID.ToString() },
+            {"Artifact Type ID", "10" },
+            {"Job ID", jobID.ToString() },
+            {"Application Name", applicationName },
+            {"Correlation ID", correlationID }
+        });
+
+        // Export source settings
+        var sourceSettings = ExportSourceSettingsBuilder.Create()
+            .FromFolder(exportSourceArtifactID: folderID, viewID: viewID)
+            .WithSubfolders() // include subfolders
+            .WithCustomStartAtDocumentNumber(1)
+            .Build();
+
+        // Select and assign an order to long text fields that Relativity checks for text when performing an export.
+        // You must provide at least one long text field to use this functionality
+        // If there won't be a value for a field, the next field in the list will be used
+        // Example:
+        // 1003668 - Extracted Text
+        // 1003677 - Folder Name
+        // If there won't be any value for Extracted Text, Folder Name will be used
+        List<int> fulltextPrecedenceFieldsArtifactIds = new() { 1003668, 1003677 };
+
+        // Aliases can use only artifact IDs that are in the fieldArtifactIDs list
+        // Key is the artifact ID of the field, value is the alias
+        List<int> fieldArtifactIDs = new() { 1003676, 1003667 };
+        Dictionary<int, string> fieldAliases = new()
+        {
+            { 1003676, "Field1" },
+            { 1003667, "Field2" }
+        };
+
+        // Artifact settings
+        var artifactSettings = ExportArtifactSettingsBuilder.Create()
+            .WithDefaultFileNamePattern()
+            .WithoutApplyingFileNamePatternToImages()
+            .ExportImages(settings => settings
+                .WithImagePrecedenceArtifactIDs(new List<int> { -1 }) // Exports images
+                .WithTypeOfImage(ImageType.Pdf))
+            .ExportFullText(settings => settings
+                .ExportFullTextAsFile()
+                .WithTextFileEncoding("UTF-8")
+                .WithPrecedenceFieldsArtifactIDs(fulltextPrecedenceFieldsArtifactIds))
+            .ExportNative(settings => settings
+                .WithNativePrecedenceArtifactIDs(new List<int> { -1 })) // Exports native files
+            .ExportPdf() // Export PDF files
+            .WithFieldArtifactIDs(fieldArtifactIDs) // Fields to export
+            .WithFieldAliases(fieldAliases)
+            .WithoutExportingMultiChoicesAsNested()
+            .Build();
+
+        // Subdirectory settings
+        var subdirectorySettings = SubdirectorySettingsBuilder.Create()
+            .WithSubdirectoryStartNumber(1)
+            .WithMaxNumberOfFilesInDirectory(100)
+            .WithDefaultPrefixes()
+            .OverridePrefixDefaults(prefixes =>
+            {
+                // Optional overrides
+                prefixes.FullTextSubdirectoryPrefix = "FULLTEXT_";
+                prefixes.NativeSubdirectoryPrefix = "NATIVE_";
+                prefixes.ImageSubdirectoryPrefix = "IMAGE_";
+                prefixes.PdfSubdirectoryPrefix = "PDF_";
+            })
+            .WithSubdirectoryDigitPadding(5)
+            .Build();
+
+        // Volume settings
+        var volumeSettings = VolumeSettingsBuilder.Create()
+            .WithVolumePrefix("VOL_FOLDER_")
+            .WithVolumeStartNumber(1)
+            .WithVolumeMaxSizeInMegabytes(100)
+            .WithVolumeDigitPadding(5)
+            .Build();
+
+        // Loadfile settings
+        var loadfileSettings = LoadFileSettingsBuilder.Create()
+            .WithoutExportingMsAccess()
+            .WithoutCustomCultureInfo()
+            .WithDefaultDateTimeFormat()
+            .WithLoadFileFormat(LoadFileFormat.CSV)
+            .WithEncoding("UTF-8")
+            .WithImageLoadFileFormat(ImageLoadFileFormat.IPRO)
+            .WithPdfFileFormat(PdfLoadFileFormat.IPRO_FullText)
+            .WithDelimiterSettings(delimiters =>
+                delimiters.WithDefaultDelimiters())
+            .Build();
+
+        // Output settings
+        var outputSettings = ExportOutputSettingsBuilder.Create()
+            .WithoutArchiveCreation()
+            .WithDefaultFolderStructure()
+            .WithoutTransferJobID()
+            .WithDefaultDestinationPath()
+            .WithSubdirectorySettings(subdirectorySettings)
+            .WithVolumeSettings(volumeSettings)
+            .WithLoadFileSettings(loadfileSettings)
+            .Build();
+
+        // Connect all settings in the Job builder
+        var jobSettings = ExportJobSettingsBuilder.Create()
+            .WithExportSourceSettings(sourceSettings)
+            .WithExportArtifactSettings(artifactSettings)
+            .WithExportOutputSettings(outputSettings)
+            .Build();
+
+        _logger.PrintAliases(fieldAliases);
+
+        // Create proxy to use IExportJobManager
+        using Relativity.Export.V1.IExportJobManager jobManager = _serviceFactory.CreateProxy<Relativity.Export.V1.IExportJobManager>();
+
+        _logger.PrintJobJson(jobSettings);
+
+        // Create export job
+        _logger.LogInformation("Creating job");
+        var validationResult = await jobManager.CreateAsync(
+            workspaceID,
+            jobID,
+            jobSettings,
+            applicationName,
+            correlationID);
+
+        if (validationResult is null)
+        {
+            _logger.LogError("Something went wrong. Received empty response.");
+            return;
+        }
+
+        // check validation result
+        if (!validationResult.IsSuccess)
+        {
+            _logger.LogError($"<{validationResult.ErrorCode}> {validationResult.ErrorMessage}");
+
+            // iterate errors and print them
+            foreach (var validationError in validationResult.Value.ValidationErrors)
+            {
+                _logger.LogError($"{validationError.Key} - {validationError.Value}");
+            }
+
+            return;
+        }
+
+        _logger.LogInformation("Job created successfully");
+
+        // Start export job
+        _logger.LogInformation($"Stating job with <{jobID}> ID");
+        var startResponse = await jobManager.StartAsync(workspaceID, jobID);
+
+        // Check for errors that occured during job start
+        if (!string.IsNullOrEmpty(startResponse.ErrorMessage))
+        {
+            _logger.LogError($"<{startResponse.ErrorCode}> {startResponse.ErrorMessage}");
+
+            return;
+        }
+
+        // Get status of the job and await for the completed state
+        _logger.LogInformation("Awaiting job status updates");
+        var jobResult = await WaitForJobToBeCompletedAsync(async () =>
+        {
+            return await jobManager.GetAsync(workspaceID, jobID);
+        });
+
+        string resultData =
+                $"Export job ID: {jobResult.ExportJobID}\n"
+                + $"Correlation ID: {jobResult.Value.CorrelationID}\n"
+                + $"Job status: {jobResult.Value.JobStatus}\n"
+                + $"Job error count: {jobResult.Value.JobErrorsCount}\n"
+                + $"Total records: {jobResult.Value.TotalRecords}\n"
+                + $"Processed records: {jobResult.Value.ProcessedRecords}\n"
+                + $"Exported files count: {jobResult.Value.ExportedFilesCount}\n"
+                + $"Total size of exported files: {jobResult.Value.TotalSizeOfExportedFiles}\n"
+                + $"Records with warnings: {jobResult.Value.RecordsWithErrors}\n"
+                + $"Records with errors: {jobResult.Value.RecordsWithErrors}\n"
+                + $"Output URL: [orange1]{jobResult.Value.ExportJobOutput.OutputUrl}[/]";
+
+        _logger.LogInformation("Job Completed");
+        _logger.PrintExportJobResult(resultData, jobResult.Value);
+    }
+}
